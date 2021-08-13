6b5e2adc36a38855f38563662bc4949b2ab51bd4
Keeeean <suk@macstudent.com.au>
2021-08-13 22:43:24 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function startSim() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("cpu").style.display = "block";
+    document.getElementById("cpu_image").style.display = "block";
+
+}
+
+function nextRam() {
+    document.getElementById("ram").style.display = "block";
+    document.getElementById("cpu").style.display = "none";
+    document.getElementById("cpu_image").style.display = "none";
+    document.getElementById("task_man").style.display ="block";
+
+}
+
