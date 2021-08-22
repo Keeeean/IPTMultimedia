b030cd51a4fae6f4bc348a27bcb6d6c8c3479d6e
Keeeean <suk@macstudent.com.au>
2021-08-22 19:35:58 +1000

    worked on gpu

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ function nextRam() {
     document.getElementById("cpu").style.display = "none";
     document.getElementById("cpu_image").style.display = "none";
     document.getElementById("task_man").style.display ="block";
+    document.getElementById("set1").style.display= "block";
+    document.getElementById("ram_image").style.display = "block";
 
 }
 
+function nextGPU() {
+    document.getElementById("ram").style.display = "none";
+    document.getElementById("gpu").style.display = "block";
+    document.getElementById("set1").style.display = "none";
+    document.getElementById("ram_image").style.display ="none";
+
+}
+
+function backCPU() {
+
+}
