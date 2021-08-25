813654ae13c2534ca9cac0f83b07319a5075f86d
Keeeean <suk@macstudent.com.au>
2021-08-25 11:55:33 +1000

    finished gpu and related js, will now go onto finishing the motherboard component of the task and work on the finishing screen later.

--- main.js
+++ main.js
@@ -20,9 +20,25 @@ function nextGPU() {
     document.getElementById("gpu").style.display = "block";
     document.getElementById("set1").style.display = "none";
     document.getElementById("ram_image").style.display ="none";
+    document.getElementById("set2").style.display = "block";
+    document.getElementById("gpu_images").style.display = "block";
 
 }
 
 function backCPU() {
+    document.getElementById("ram").style.display = "none";
+    document.getElementById("cpu").style.display = "block";
+    document.getElementById("set1").style.display = "none";
+    document.getElementById("ram_image").style.display= "none";
+    document.getElementById("cpu_image").style.display = "block";
 
+}
+
+function backRAM() {
+    document.getElementById("gpu").style.display = "none";
+    document.getElementById("ram").style.display = "block";
+    document.getElementById("set1").style.display = "block";
+    document.getElementById("set2").style.display = "none";
+    document.getElementById("ram_image").style.display = "block";
+    document.getElementById("gpu_images").style.display = "none";
 }
