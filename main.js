e09efed6e86bfc8cf045660ee8b9b4a119fe2043
Keeeean <suk@macstudent.com.au>
2021-08-29 19:35:02 +1000

    finished the entire project now. added the finish screen and the function to revisit older parts.

--- main.js
+++ main.js
@@ -42,3 +42,64 @@ function backRAM() {
     document.getElementById("ram_image").style.display = "block";
     document.getElementById("gpu_images").style.display = "none";
 }
+
+function nextMB() {
+    document.getElementById("gpu").style.display = "none";
+    document.getElementById("set2").style.display = "none";
+    document.getElementById("set3").style.display = "block";
+    document.getElementById("motherboard").style.display = "block";
+    document.getElementById("gpu_images").style.display = "none";
+    document.getElementById("motherboard_img").style.display = "block";
+
+}
+
+function backGPU() {
+    document.getElementById("gpu").style.display = "block";
+    document.getElementById("motherboard").style.display = "none";
+    document.getElementById("set2").style.display = "block";
+    document.getElementById("gpu_images").style.display = "block";
+    document.getElementById("set3").style.display = "none";
+
+
+}
+
+function finish() {
+    document.getElementById("motherboard").style.display = "none";
+    document.getElementById("set3").style.display = "none";
+    document.getElementById("finish_screen").style.display = "block";
+    document.getElementById("set4").style.display = "block";
+
+}
+
+function CPU() {
+    document.getElementById("finish_screen").style.display = "none";
+    document.getElementById("set4").style.display = "none";
+    document.getElementById("cpu").style.display = "block";
+    document.getElementById("cpu_image").style.display = "block";
+}
+
+function RAM() {
+    document.getElementById("finish_screen").style.display = "none";
+    document.getElementById("set4").style.display = "none";
+    document.getElementById("ram").style.display = "block";
+    document.getElementById("ram_image").style.display = "block";
+    document.getElementById("set1").style.display = "block";
+
+}
+
+function GPU() {
+    document.getElementById("finish_screen").style.display = "none";
+    document.getElementById("set4").style.display = "none";
+    document.getElementById("gpu").style.display = "block";
+    document.getElementById("gpu_images").style.display = "block";
+    document.getElementById("set2").style.display = "block";
+}
+
+function Motherboard() {
+    document.getElementById("finish_screen").style.display = "none";
+    document.getElementById("set4").style.display = "none";
+    document.getElementById("set3").style.display = "block";
+    document.getElementById("motherboard").style.display = "block";
+    document.getElementById("motherboard_img").style.display = "block";
+
+}
