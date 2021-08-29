9672e20565a9db072a99c4ba615996aad98b0066
Keeeean <suk@macstudent.com.au>
2021-08-30 09:57:20 +1000

    creditted the video and images in the website.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function startSim() {
     document.getElementById("start").style.display = "none";
     document.getElementById("cpu").style.display = "block";
-    document.getElementById("cpu_image").style.display = "block";
+    document.getElementById("cpu_images").style.display = "block";
 
 }
 
 function nextRam() {
     document.getElementById("ram").style.display = "block";
     document.getElementById("cpu").style.display = "none";
-    document.getElementById("cpu_image").style.display = "none";
+    document.getElementById("cpu_images").style.display = "none";
     document.getElementById("task_man").style.display ="block";
     document.getElementById("set1").style.display= "block";
-    document.getElementById("ram_image").style.display = "block";
+    document.getElementById("ram_images").style.display = "block";
 
 }
 
@@ -19,7 +19,7 @@ function nextGPU() {
     document.getElementById("ram").style.display = "none";
     document.getElementById("gpu").style.display = "block";
     document.getElementById("set1").style.display = "none";
-    document.getElementById("ram_image").style.display ="none";
+    document.getElementById("ram_images").style.display ="none";
     document.getElementById("set2").style.display = "block";
     document.getElementById("gpu_images").style.display = "block";
 
@@ -29,8 +29,8 @@ function backCPU() {
     document.getElementById("ram").style.display = "none";
     document.getElementById("cpu").style.display = "block";
     document.getElementById("set1").style.display = "none";
-    document.getElementById("ram_image").style.display= "none";
-    document.getElementById("cpu_image").style.display = "block";
+    document.getElementById("ram_images").style.display= "none";
+    document.getElementById("cpu_images").style.display = "block";
 
 }
 
@@ -39,7 +39,7 @@ function backRAM() {
     document.getElementById("ram").style.display = "block";
     document.getElementById("set1").style.display = "block";
     document.getElementById("set2").style.display = "none";
-    document.getElementById("ram_image").style.display = "block";
+    document.getElementById("ram_images").style.display = "block";
     document.getElementById("gpu_images").style.display = "none";
 }
 
@@ -59,6 +59,7 @@ function backGPU() {
     document.getElementById("set2").style.display = "block";
     document.getElementById("gpu_images").style.display = "block";
     document.getElementById("set3").style.display = "none";
+    document.getElementById("motherboard_img").style.display = "none";
 
 
 }
@@ -68,6 +69,7 @@ function finish() {
     document.getElementById("set3").style.display = "none";
     document.getElementById("finish_screen").style.display = "block";
     document.getElementById("set4").style.display = "block";
+    document.getElementById("motherboard_img").style.display = "none";
 
 }
 
@@ -75,14 +77,14 @@ function CPU() {
     document.getElementById("finish_screen").style.display = "none";
     document.getElementById("set4").style.display = "none";
     document.getElementById("cpu").style.display = "block";
-    document.getElementById("cpu_image").style.display = "block";
+    document.getElementById("cpu_images").style.display = "block";
 }
 
 function RAM() {
     document.getElementById("finish_screen").style.display = "none";
     document.getElementById("set4").style.display = "none";
     document.getElementById("ram").style.display = "block";
-    document.getElementById("ram_image").style.display = "block";
+    document.getElementById("ram_images").style.display = "block";
     document.getElementById("set1").style.display = "block";
 
 }
